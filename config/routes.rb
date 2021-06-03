077b616bfac9f1167d61f91c062d4d6c7ddad673
Prakrityrathore <63180062+Prakrityrathore@users.noreply.github.com>
2021-06-03 12:09:15 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:show, :index, :create, :update, :destroy ]
-  resources :dishes, only: [:index, :create, :update, :destroy]
+  resources :restaurants, only: [:show, :index, :create, :update, :destroy ] do
+    resources :dishes, only: [:index, :create, :update, :destroy]
 end
